746eaa08d129ca96b79cbd080c9dab15357da739
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-01-05 10:57:32 -0500

    Initial commit

---
+++ test/amplify-rds-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AmplifyRdsApp from '../lib/amplify-rds-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/amplify-rds-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AmplifyRdsApp.AmplifyRdsAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
